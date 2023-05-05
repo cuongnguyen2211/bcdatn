d29b09567ecf180178204c9e41e72babeeb70d36
cuongnguyen2211 <jorkerluv1@gmail.com>
2023-05-05 17:12:36 +0700

    init project

---
+++ inc/footer.php
@@ -0,0 +1,93 @@
+<footer>
+  <div class="footer-wrappper section-bg">
+    <div class="footer-area footer-padding">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-xl-3 col-lg-5 col-md-4 col-sm-6">
+            <div class="single-footer-caption mb-50">
+              <div class="single-footer-caption mb-30">
+                <div class="footer-logo mb-25">
+                  <a href="index.php">
+                    
+                    <img  alt="" data-pagespeed-url-hash="2868754985" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" /></a>
+                </div>
+                <div class="footer-tittle">
+                  <div class="footer-pera">
+                    <p>
+                      A person who won’t read has no advantage over one who can’t read. <br>– Mark Twain –
+                    </p>
+                    
+
+                  </div>
+                </div>
+
+                <div class="footer-social">
+                  <a href="https://facebook.com/tcdbook" target="_blank"><i class="fab fa-facebook" style="margin-top: 12px"></i></a>
+                  <a href="#"><i class="fab fa-instagram" style="margin-top: 12px"></i></a>
+                  <a href="#"><i class="fab fa-linkedin-in" style="margin-top: 12px"></i></a>
+                  <a href="#"><i class="fab fa-youtube" style="margin-top: 12px"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
+            <div class="single-footer-caption mb-50">
+              <div class="footer-tittle">
+                <h4>Categories</h4>
+                <ul>
+                  <li><a href="categories.php?cate_id%5B%5D=1">Literature</a></li>
+                  <li><a href="#">economic book</a></li>
+                  <li><a href="#">Children's books</a></li>
+                  <li><a href="categories.php?cate_id%5B%5D=4">Life skills book</a></li>
+                  <li><a href="#">English book</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+            <div class="single-footer-caption mb-50">
+              <div class="footer-tittle">
+                <h4>Book</h4>
+                <ul>
+                  <li><a href="#">History book</a></li>
+                  <li><a href="#">Information technology book</a></li>
+                  <li><a href="#">Comics, manga</a></li>
+                  <li><a href="#">Book of exercise - sport</a></li>
+                  <li><a href="#">Psychology - sex book </a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+            <div class="single-footer-caption mb-50">
+              <div class="footer-tittle">
+                <h4>Site Map</h4>
+                <ul class="mb-20">
+                  <li><a href="./index.php">Home</a></li>
+                  <li><a href="./about.php">About</a></li>
+                  <li><a href="#">FAQs</a></li>
+                  <li><a href="./blog.php">Blog</a></li>
+                  <li><a href="./contact.php">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-bottom-area">
+      <div class="container">
+        <div class="footer-border">
+          <div class="row d-flex align-items-center">
+            <div class="col-xl-12">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
